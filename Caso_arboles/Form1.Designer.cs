83011313023866aa6b86efaa795ea85904f69610
Andresmejia62 <amejia@uamv.edu.ni>
2025-11-15 16:44:45 -0600

    Agregar archivos de proyecto.

---
+++ Caso_arboles/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Caso_arboles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDatos = new System.Windows.Forms.TextBox();
+            this.btInsertar = new System.Windows.Forms.Button();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.btRecorrer = new System.Windows.Forms.Button();
+            this.btContar = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbDatos
+            // 
+            this.tbDatos.Location = new System.Drawing.Point(76, 38);
+            this.tbDatos.Name = "tbDatos";
+            this.tbDatos.Size = new System.Drawing.Size(264, 26);
+            this.tbDatos.TabIndex = 0;
+            this.tbDatos.TextChanged += new System.EventHandler(this.tbDatos_TextChanged);
+            // 
+            // btInsertar
+            // 
+            this.btInsertar.Location = new System.Drawing.Point(6, 25);
+            this.btInsertar.Name = "btInsertar";
+            this.btInsertar.Size = new System.Drawing.Size(75, 66);
+            this.btInsertar.TabIndex = 1;
+            this.btInsertar.Text = "Insertar";
+            this.btInsertar.UseVisualStyleBackColor = true;
+            this.btInsertar.Click += new System.EventHandler(this.btInsertar_Click);
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(249, 27);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(75, 66);
+            this.btBuscar.TabIndex = 2;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // btRecorrer
+            // 
+            this.btRecorrer.Location = new System.Drawing.Point(87, 30);
+            this.btRecorrer.Name = "btRecorrer";
+            this.btRecorrer.Size = new System.Drawing.Size(75, 61);
+            this.btRecorrer.TabIndex = 3;
+            this.btRecorrer.Text = "Recorrer";
+            this.btRecorrer.UseVisualStyleBackColor = true;
+            this.btRecorrer.Click += new System.EventHandler(this.btRecorrer_Click);
+            // 
+            // btContar
+            // 
+            this.btContar.Location = new System.Drawing.Point(168, 30);
+            this.btContar.Name = "btContar";
+            this.btContar.Size = new System.Drawing.Size(75, 61);
+            this.btContar.TabIndex = 4;
+            this.btContar.Text = "Contar";
+            this.btContar.UseVisualStyleBackColor = true;
+            this.btContar.Click += new System.EventHandler(this.btContar_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(20, 111);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(290, 205);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btInsertar);
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Controls.Add(this.btRecorrer);
+            this.groupBox1.Controls.Add(this.btBuscar);
+            this.groupBox1.Controls.Add(this.btContar);
+            this.groupBox1.Location = new System.Drawing.Point(39, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 344);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arbol";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDatos;
+        private System.Windows.Forms.Button btInsertar;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Button btRecorrer;
+        private System.Windows.Forms.Button btContar;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Caso_arboles/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caso_arboles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Inicializar el TreeView con un nodo raíz
+            treeView1.Nodes.Add(new TreeNode("jerarquía"));
+        }
+
+
+        private void tbDatos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btInsertar_Click(object sender, EventArgs e)
+        {
+            string dato = tbDatos.Text.Trim();
+            if (string.IsNullOrEmpty(dato))
+            {
+                MessageBox.Show("Ingrese un dato válido.");
+                return;
+            }
+            TreeNode selectedNode = treeView1.SelectedNode;
+            if (selectedNode != null)
+            {
+                TreeNode newNode = new TreeNode(dato);
+                selectedNode.Nodes.Add(newNode);
+                treeView1.ExpandAll(); // Expandir para mostrar el nuevo nodo
+            }
+            else
+            {
+                // Insertar en la raíz si no hay selección
+                TreeNode newNode = new TreeNode(dato);
+                treeView1.Nodes[0].Nodes.Add(newNode);
+                treeView1.ExpandAll();
+            }
+            tbDatos.Clear(); // Limpiar el TextBox después de insertar
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+            // Buscar un nodo por su dato (recorrido en preorden)
+            string busqueda = tbDatos.Text.Trim();
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                MessageBox.Show("Ingrese un dato para buscar.");
+                return;
+            }
+            TreeNode encontrado = BuscarNodo(treeView1.Nodes, busqueda);
+            if (encontrado != null)
+            {
+                treeView1.SelectedNode = encontrado;
+                MessageBox.Show("Nodo encontrado y seleccionado.");
+            }
+            else
+            {
+                MessageBox.Show("Nodo no encontrado.");
+            }
+        }
+        private TreeNode BuscarNodo(TreeNodeCollection nodes, string dato)
+        {
+            // Método recursivo para buscar un nodo
+            foreach (TreeNode node in nodes)
+            {
+                if (node.Text.Equals(dato, StringComparison.OrdinalIgnoreCase))
+                    return node;
+                TreeNode hijo = BuscarNodo(node.Nodes, dato);
+                if (hijo != null) return hijo;
+            }
+            return null;
+        }
+
+        private void btRecorrer_Click(object sender, EventArgs e)
+        {
+            // Recorrer el árbol en preorden y mostrar los datos en un MessageBox
+            List<string> recorrido = new List<string>();
+            Recorrer(treeView1.Nodes, recorrido);
+            if (recorrido.Count > 0)
+            {
+                MessageBox.Show("Recorrido en preorden: " + string.Join(" -> ", recorrido));
+            }
+            else
+            {
+                MessageBox.Show("El árbol está vacío.");
+            }
+        }
+        private void Recorrer(TreeNodeCollection nodes, List<string> lista)
+        {
+            // Método recursivo para recorrido en preorden
+            foreach (TreeNode node in nodes)
+            {
+                lista.Add(node.Text);
+                Recorrer(node.Nodes, lista);
+            }
+        }
+
+        private void btContar_Click(object sender, EventArgs e)
+        {
+            // Contar nodos totales y calcular el número máximo de niveles
+            int totalNodos = ContarNodos(treeView1.Nodes);
+            int maxNiveles = NivelMax(treeView1.Nodes, 0);
+            MessageBox.Show($"Total de nodos: {totalNodos}\nNúmero máximo de niveles: {maxNiveles}");
+
+        }
+        private int ContarNodos(TreeNodeCollection nodes)
+        {
+            // Contar nodos recursivamente
+            int contador = 0;
+            foreach (TreeNode node in nodes)
+            {
+                contador++;
+                contador += ContarNodos(node.Nodes);
+            }
+            return contador;
+        }
+        private int NivelMax(TreeNodeCollection nodes, int nivelActual)
+        {
+            // Calcular el nivel máximo recursivamente
+            int max = nivelActual;
+            foreach (TreeNode node in nodes)
+            {
+                int maxHijo = NivelMax(node.Nodes, nivelActual + 1);
+                if (maxHijo > max) max = maxHijo;
+            }
+            return max;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+}
